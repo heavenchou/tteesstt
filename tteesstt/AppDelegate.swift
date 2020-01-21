ecc904a6c380e7a00ef77d6408b5f1939f265eda
Heaven Chou <heaven.chou@gmail.com>
2020-01-22 01:20:51 +0800

    Initial Commit

---
+++ tteesstt/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  tteesstt
+//
+//  Created by Heaven Chou on 2020/1/22.
+//  Copyright © 2020 CBETA. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ tteesstt/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  tteesstt
+//
+//  Created by Heaven Chou on 2020/1/22.
+//  Copyright © 2020 CBETA. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
